55c1b3b5ac7c2b4eb1a1143d3cc40580ec9247f8
ShahdMarouf <shahdmarouf668@gmail.com>
2025-03-10 11:33:58 +0200

    Fix: Added getPrerenderParams to fix prerendering issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,28 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  
+  {
+    path:'checkout/:id',
+    renderMode:RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '123' },
+      { id: '456' }
+    ]
+  }
+  ,
+  {
+    path:'details/:id',
+    renderMode:RenderMode.Prerender,
+     getPrerenderParams: async () => [
+      { id: '789' },
+      { id: '101' }
+    ]
+  }
+  ,
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+ 
 ];
